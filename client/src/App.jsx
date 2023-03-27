ed9a3e7aac79a158b3e3dc9729084f59b13f28e6
Julie Nisbet <jmnisbet@gmail.com>
2023-03-26 20:10:35 -0700

    add API route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
-  return (
-    <>
-      <h1> Hello my friends!!</h1>
-    </>
-  );
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetch('/api/example');
+      const data = await resp.json();
+      console.log(data);
+      setMessage(data.message);
+    };
+    fetchData();
+  }, []);
+  return <h1> {message}</h1>;
 }
